2cff514cd45bb9c3bdd10fe6d3ddb94c1d8e382e
woobin <ss96232@naver.com>
2025-10-29 16:21:05 +0900

    10/29 add default config, cdn error fixed"

--- client/preload.js
+++ client/preload.js
@@ -1,9 +1,40 @@
 // 브리지
 
 const { contextBridge, ipcRenderer } = require("electron");
+const fs = require("fs");
+const path = require("path");
+
+function readJsonSafe(p) {
+  try {
+    return JSON.parse(fs.readFileSync(p, "utf-8"));
+  } catch {
+    return null;
+  }
+}
+
+const PD = "C:\\ProgramData\\Rad Messenger\\client\\config.json";
+const APPDATA = process.env.LOCALAPPDATA
+  ? path.join(process.env.LOCALAPPDATA, "Rad Messenger", "config.json")
+  : null;
+const RES_DEFAULT = path.join(process.resourcesPath, "config.default.json"); // extraResources 로 포함
+
+async function loadConfig() {
+  return (
+    (PD && readJsonSafe(PD)) ||
+    (APPDATA && readJsonSafe(APPDATA)) ||
+    readJsonSafe(RES_DEFAULT) || {
+      serverUrl: "http://192:168.10.70:3030",
+      displayName: "Unknown",
+      defaultRoom: "1",
+      alwaysOnTop: false,
+      soundEnabled: true,
+      soundVolume: 1.0,
+    }
+  );
+}
 
 contextBridge.exposeInMainWorld("api", {
-  loadConfig: () => ipcRenderer.invoke("config:load"),
+  loadConfig,
   setAlwaysOnTop: async (flag) =>
     ipcRenderer.invoke("win:set-always-on-top", !!flag),
 });
